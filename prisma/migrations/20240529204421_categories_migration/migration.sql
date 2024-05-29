3b2761c3cf5fedbecb8b31c36e0a99f4f0ed8f40
Gubrux <gabrielcentur@gmail.com>
2024-05-29 17:30:44 -0400

    feat: Add migrations for Category and Product tables
    This commit adds the necessary SQL migration scripts to create the "Category" and "Product" tables in the database. The "Category" table has columns for "id", "name", and "slug", while the "Product" table has columns for "id", "name", "price", "image", and "categoryId". The "Product" table also has a foreign key constraint referencing the "id" column of the "Category" table.

---
+++ prisma/migrations/20240529204421_categories_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20240529204916_products_migration/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
