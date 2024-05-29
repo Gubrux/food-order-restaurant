a650d78371a394d426454de3eee5941b23a9899e
Gubrux <gabrielcentur@gmail.com>
2024-05-29 17:34:31 -0400

    feat: Add categories seed data

---
+++ prisma/data/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+    {
+        slug: "cafe",
+        name: "Café",
+    },
+    {
+        slug: "hamburguesa",
+        name: "Hamburguesas",
+    },
+    {
+        slug: "pizza",
+        name: "Pizzas",
+    },
+    {
+        slug: "dona",
+        name: "Donas",
+    },
+    {
+        slug: "pastel",
+        name: "Pasteles",
+    },
+    {
+        slug: "galletas",
+        name: "Galletas",
+    },
+];
