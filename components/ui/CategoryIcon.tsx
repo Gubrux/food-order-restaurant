17a6b2a4f084b9ccc873e9cc46d85047984822e7
Gubrux <gabrielcentur@gmail.com>
2024-05-29 18:16:43 -0400

    Update CategoryIcon component to include image and link

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,8 +1,29 @@
 import { Category } from "@prisma/client";
-
+import Link from "next/link";
+import Image from "next/image";
 type CategoryIconProps = {
     category: Category;
 };
 export default function CategoryIcon({ category }: CategoryIconProps) {
-    return <div>{category.name}</div>;
+    return (
+        <>
+            <div
+                className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+            >
+                <div className="w-16 h-16 relative">
+                    <Image
+                        fill
+                        src={`/icon_${category.slug}.svg`}
+                        alt="Imagen Categoria"
+                    />
+                </div>
+                <Link
+                    href={`/order/${category.slug}`}
+                    className="font-bold text-xl"
+                >
+                    {category.name}
+                </Link>
+            </div>
+        </>
+    );
 }
