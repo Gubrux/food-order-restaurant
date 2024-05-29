e53947c821c31256b051029e286515f752eaf4e9
Gubrux <gabrielcentur@gmail.com>
2024-05-29 17:34:18 -0400

    feat: Add OrderSummary component

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,9 @@
+export default function OrderSummary() {
+    return (
+        <>
+            <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+                <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+            </aside>
+        </>
+    );
+}
