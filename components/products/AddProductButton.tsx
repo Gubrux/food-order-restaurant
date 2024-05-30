1f0a6b60bdd94e629b10eabba5a966837da5adc5
Gubrux <gabrielcentur@gmail.com>
2024-05-30 17:45:43 -0400

    Add AddProductButton component for adding a product to the order

---
+++ components/products/AddProductButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { useStore } from "@/src/store";
+type AddProductButtonProps = {
+    product: Product;
+};
+export default function AddProductButton({ product }: AddProductButtonProps) {
+    const addToOrder = useStore((state) => state.addToOrder);
+    return (
+        <button
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+            onClick={() => addToOrder(product)}
+        >
+            Agregar
+        </button>
+    );
+}
