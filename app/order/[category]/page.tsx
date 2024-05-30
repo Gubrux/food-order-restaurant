b6ca8271af7ee98f5af6ff9b465ff047991fd1bd
Gubrux <gabrielcentur@gmail.com>
2024-05-30 12:46:59 -0400

    Update OrderPage to dynamically fetch and display products by category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,8 +1,31 @@
-export default function OrderPage({
+import ProductCard from "@/components/products/ProductCard";
+import { prisma } from "@/src/lib/prisma";
+
+async function getProducts(category: string) {
+    const products = await prisma.product.findMany({
+        where: {
+            category: {
+                slug: category,
+            },
+        },
+    });
+    return products;
+}
+
+export default async function OrderPage({
     params,
 }: {
     params: { category: string };
 }) {
-    console.log(params.category);
-    return <div>OrderPage</div>;
+    const products = await getProducts(params.category);
+    return (
+        <>
+            <h1 className="text-2xl my-10">Realiza tu pedido</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </>
+    );
 }
