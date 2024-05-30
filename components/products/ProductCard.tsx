70c67ba64afdad47ccc2c07b7982e3743375a55e
Gubrux <gabrielcentur@gmail.com>
2024-05-30 17:45:54 -0400

    Add AddProductButton component for adding a product to the order

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { formatCurrency } from "@/src/utils";
 import { Product } from "@prisma/client";
 import Image from "next/image";
+import AddProductButton from "./AddProductButton";
 
 type ProductCardProps = {
     product: Product;
@@ -22,9 +23,7 @@ export default function ProductCard({ product }: ProductCardProps) {
                     <p className="mt-5 font-black text-4xl text-amber-500">
                         {formatCurrency(product.price)}
                     </p>
-                    <button className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer">
-                        Agregar
-                    </button>
+                    <AddProductButton product={product} />
                 </div>
             </div>
         </>
