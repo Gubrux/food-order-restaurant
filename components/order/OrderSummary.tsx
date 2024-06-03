243209ee4a2bf2aeb9ea71b3829978ecc47b6172
Gubrux <gabrielcentur@gmail.com>
2024-06-03 16:49:07 -0400

    Update OrderSummary component to display order details and total amount

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -18,7 +18,7 @@ export default function OrderSummary() {
             <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
                 <h1 className="text-4xl text-center font-black">Mi pedido</h1>
                 {order.length === 0 ? (
-                    <p className="text-center my-10"> El carrito esta vacio</p>
+                    <p className="text-center my-10"> El pedido esta vacio</p>
                 ) : (
                     <div className="mt-5">
                         {order.map((item) => (
@@ -27,10 +27,17 @@ export default function OrderSummary() {
 
                         <p className="text-2xl mt-20 text-center">
                             Total a pagar: {""}
-                        <span className="font-bold text-center">
-                            {formatCurrency(total)}
-                        </span>
+                            <span className="font-bold text-center">
+                                {formatCurrency(total)}
+                            </span>
                         </p>
+                        <form className="w-full mt-10 space-y-5" action="">
+                            <input
+                                type="submit"
+                                className="py-2 rounded uppercase text-white bg-slate-900 w-full font-bold text-center cursor-pointer hover:bg-slate-800"
+                                value={`Confirmar Pedido`}
+                            />
+                        </form>
                     </div>
                 )}
             </aside>
---
+++ prisma/migrations/20240530222957_order_products/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "orderReadyAt" TIMESTAMP(3),
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProducts" (
+    "id" SERIAL NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderProducts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
